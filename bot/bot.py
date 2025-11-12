bacd07930b3019de06aeeaaac45a5fc617240afb
Владимир Полозов <57739258+VladimirPolozov@users.noreply.github.com>
2025-11-12 08:51:02 +0700

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,6 @@ from config import MAX_BOT_TOKEN
 from external.deepseek_client import DeepSeekClient
 from core.knowledge_service import KnowledgeService
 from bot.handlers.on_start import send_welcome
-from bot.handlers.on_message import handle_user_message
 
 def create_bot() -> aiomax.Bot:
     bot = aiomax.Bot(MAX_BOT_TOKEN, default_format="markdown")
@@ -16,3 +15,4 @@ def create_bot() -> aiomax.Bot:
         await handle_user_message(msg, knowledge_service)
 
     return bot
+
